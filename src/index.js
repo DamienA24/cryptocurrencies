1953add6cb3e936e3945d69cc19e731353a55492
DamienA24 <alves.d24@laposte.net>
2020-10-21 23:43:30 +0200

    feat(index): check ping and arguments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import express from "express";
-import { displayLog } from "./utils/log.js";
+
+import { getCryptosList } from "./modules/Crypto/controller.js";
+import { requestHandler } from "./modules/Axios/index.js";
+
+import { displayLog, numberArgs, shutServer } from "./utils/index.js";
 
 const app = express();
 const port = 4000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
   displayLog("yellow", `server listening on port ${port}`);
+  checkPing();
+  checkArgs();
 });
+
+async function checkPing() {
+  const resultPing = await requestHandler("ping");
+  if (resultPing.status !== 200) {
+    shutServer();
+  }
+}
+
+async function checkArgs() {
+  const hasArgs = numberArgs();
+  if (hasArgs.length) {
+    const list = await getCryptosList();
+  } else {
+    displayLog("blue", "missing arguments, try again");
+    shutServer();
+  }
+}
