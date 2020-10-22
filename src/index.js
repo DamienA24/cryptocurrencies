b027d409d2545a1e8de66f71ba3af10640d3d9a5
DamienA24 <alves.d24@laposte.net>
2020-10-22 14:59:25 +0200

    refactor(index): add check

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 
-import { getCryptosList } from "./modules/Crypto/controller.js";
+import {
+  findSelectedCryptos,
+  getCryptosList,
+} from "./modules/Crypto/controller.js";
 import { requestHandler } from "./modules/Axios/index.js";
 
-import { displayLog, numberArgs, shutServer } from "./utils/index.js";
+import {
+  formateArgs,
+  displayLog,
+  numberArgs,
+  shutServer,
+} from "./utils/index.js";
 
 const app = express();
 const port = 4000;
@@ -11,20 +19,24 @@ const port = 4000;
 app.listen(port, async () => {
   displayLog("yellow", `server listening on port ${port}`);
   checkPing();
-  checkArgs();
+  checkResearch();
 });
 
 async function checkPing() {
   const resultPing = await requestHandler("ping");
   if (resultPing.status !== 200) {
+    displayLog("green", "data not available");
     shutServer();
   }
 }
 
-async function checkArgs() {
+async function checkResearch() {
   const hasArgs = numberArgs();
+  console.log(hasArgs);
   if (hasArgs.length) {
+    const formatedArgs = formateArgs(hasArgs);
     const list = await getCryptosList();
+    findSelectedCryptos(formatedArgs, list);
   } else {
     displayLog("blue", "missing arguments, try again");
     shutServer();
