f136e4cd02315b4495d59d3050419fd2dcfd8b80
DamienA24 <alves.d24@laposte.net>
2020-10-22 21:28:00 +0200

    refactor(Crypo): formate data

--- src/modules/Crypto/controller.js
+++ src/modules/Crypto/controller.js
@@ -1,5 +1,14 @@
 import { requestHandler } from "../Axios/index.js";
 import { displayLog } from "../../utils/index.js";
+import {
+  colorText,
+  createLineMain,
+  recoverDataToCatch,
+  formateCrypto,
+  createTableConfig,
+} from "./utils.js";
+import tableImport from "table";
+const { table } = tableImport;
 
 export const getCryptosList = async () => {
   const result = await requestHandler("coins/list");
@@ -28,4 +37,30 @@ export const formateSelectedCryptos = (userSearches, cryptosList) => {
     }
   );
   return formattedArgs;
+};
+
+export const getCryptosSelection = async (selection) => {
+  const { cryptosSlected } = selection;
+  const result = await requestHandler("coins/markets", "GET", {
+    vs_currency: "usd",
+    ids: cryptosSlected,
+  });
+  return result.data;
+};
+
+export const displayCryptos = (cryptos) => {
+  const data = cryptos.reduce(
+    (acc, crypto) => {
+      const dataToCapture = recoverDataToCatch();
+      const dataCaptured = formateCrypto(dataToCapture, crypto);
+      acc.push(dataCaptured);
+      return acc;
+    },
+    [createLineMain()]
+  );
+  const config = createTableConfig(2, 7, {
+    alignment: "right",
+  });
+  const output = table(data, config);
+  console.log(output);
 };
